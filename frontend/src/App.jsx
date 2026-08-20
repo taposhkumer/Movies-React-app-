3f6d44048a914dbdd7be21a492e2e9e6ef5f9c54
Taposh Kumer Ghosh <gtaposh88@gmail.com>
2026-08-20 21:35:33 +0600

    adding css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css"
+import "./css/App.css"
 import Favorites from "./pages/Favorites"
 import Home from "./pages/Home"
 import { Routes, Route } from "react-router-dom";
--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "../css/MovieCard.css"
 
 function MovieCard({ movie }) {
   const [favorite, setFavorite] = useState(false);
